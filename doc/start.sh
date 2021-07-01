504cb76f1a13a40fe0fdd666135084905f05428a
Pluto <wangmin07@hotmail.com>
2021-07-01 09:28:40 +0800

    适配修改默认端口

--- doc/start.sh
+++ doc/start.sh
@@ -13,13 +13,13 @@ export NATCROSS_CLIENT_SERVER_IP='127.0.0.1'
 export NATCROSS_CLIENT_SERVICE_PORT=10010
 
 ## 服务端web服务地址，和服务端放置的IP:${SERVER_PORT}相同，且IP和NATCROSS_CLIENT_SERVER_IP相同
-export NATCROSS_HTTP_SERVER='http://${NATCROSS_CLIENT_SERVER_IP}:10080'
+export NATCROSS_HTTP_SERVER='http://${NATCROSS_CLIENT_SERVER_IP}:10020'
 
 ## 服务端的签名key，散列算法，约等于对称密钥，客户端使用签名方式获取接口状态
 export NATCROSS_HTTP_SIGN_KEY=serverSignKey
 
 ## 交互密钥和签名key
-export NATCROSS_AES_KEY='0PMudFSqJ9WsQrTC60sva9sJAV4PF5iOBjKZW17NeF4='
+export NATCROSS_AES_KEY='1qV/BYx14agUrPFyw4ow+Q=='
 export NATCROSS_TOKEN_KEY=tokenKey
 
 JAVA_OPTS="-XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=20"
